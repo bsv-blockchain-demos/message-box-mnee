7fd84fab2ffc0a424cd1dba41e2b65efe2607032
Deggen <d.kellenschwiler@bsvassociation.org>
2025-09-28 17:04:36 -0500

    there is definitely nothing wrong with this code.

--- tests/token-transfer.test.ts
+++ tests/token-transfer.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { TokenTransfer } from '../src/mnee/TokenTransfer';
-import { Transaction, MerklePath, P2PKH, PrivateKey, UnlockingScript, Utils, Script, WalletProtocol } from '@bsv/sdk';
+import { Transaction, MerklePath, P2PKH, PrivateKey, UnlockingScript, Utils, Script, WalletProtocol, WalletClient, Hash, TransactionSignature, Signature, PublicKey } from '@bsv/sdk';
 import { MockChain, wallet } from './test-utils';
+import { getMNEEAddress } from '../src/mnee/getAddress'
 
 const cosigner = PrivateKey.fromHex('065204edc6aef2882ed9ddaaa58c4e35937249afa852fb5ed65bb3988bf1e861')
 const cosignerPubKey = cosigner.toPublicKey()
@@ -16,15 +17,7 @@ describe('Token Transfer', () => {
       unlockingScript: UnlockingScript.fromASM('3044022024731d659132cb9cf4768bdbaa3839cec56531979955ef3e4058fcd58d37274002200484424db86ad8cdb55cdc90fbddabbcf3b8386a4ceb042d0ca87834d9e2cfb141 033cd6c077be41bfc5cf83ee6a6cdc821b9247b99bb2d929490b32fc938630fac5')
     })
 
-    const { instructions, change } = {
-      instructions: {
-        protocolID: [ 2, 'Pay MNEE' ] as WalletProtocol,
-        keyID: 'MjAyNS0wOS0yNQ==',
-        counterparty: 'self',
-        forSelf: true
-      },
-      change: '1EL7jA4M4c4UFYimTz7npM25x2dQf4HcfG'
-    }
+    const { instructions, change } = await getMNEEAddress(wallet)
 
     sourceTransaction.addOutput({
       satoshis: 1,
@@ -44,7 +37,7 @@ describe('Token Transfer', () => {
     tx.addInput({
       sourceTransaction,
       sourceOutputIndex: 0,
-      unlockingScriptTemplate: new TokenTransfer().unlock(wallet, instructions, 'all', true) 
+      unlockingScriptTemplate: new TokenTransfer().unlock(wallet, instructions, 'all', true, undefined, undefined, cosigner) 
     })
     tx.addInput({
       sourceTransaction,
@@ -68,12 +61,6 @@ describe('Token Transfer', () => {
     await tx.sign()
 
     console.log({ sig: tx.inputs[0]!.unlockingScript!.toASM() })
-
-    const sig = Utils.toArray('304402206c49ddaeef9a8d8edd429658be247cdb647c844bddcd811be62cc711b493933302200411f7050b319fb6e73f3b64baaa47770679542b549497635ac8f0916c4d5d54c1', 'hex')
-    const replacement = new UnlockingScript()
-    replacement.writeBin(sig)
-    replacement.writeScript(Script.fromASM(tx.inputs[0]!.unlockingScript!.toASM()))
-    tx.inputs[0].unlockingScript = replacement
 
     const passes = await tx.verify(mockChain)
     expect(passes).toBe(true)    
